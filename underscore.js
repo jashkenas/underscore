8066c2c5dc0ac4ee1734be8005a8cbe23d299aea
Thomas Rampelberg <thomas@bittorrent.com>
2011-01-27 10:43:39 -0800

    Moving clearTimeout and setTimeout to window.*. In IE, setTimeout is slightly different from window.setTimeout (you can do assignment to window.setTimeout and not to setTimeout). By moving to window.setTimeout, it allows me reassign setTimeout to a function that wraps every setTimeout call in a try/except and provides full stack traces for any error experienced.

--- underscore.js
+++ underscore.js
@@ -436,7 +436,7 @@
   // it with the arguments supplied.
   _.delay = function(func, wait) {
     var args = slice.call(arguments, 2);
-    return setTimeout(function(){ return func.apply(func, args); }, wait);
+    return window.setTimeout(function(){ return func.apply(func, args); }, wait);
   };
 
   // Defers a function, scheduling it to run after the current call stack has
@@ -454,8 +454,8 @@
         timeout = null;
         func.apply(context, args);
       };
-      if (debounce) clearTimeout(timeout);
-      if (debounce || !timeout) timeout = setTimeout(throttler, wait);
+      if (debounce) window.clearTimeout(timeout);
+      if (debounce || !timeout) timeout = window.setTimeout(throttler, wait);
     };
   };
 
