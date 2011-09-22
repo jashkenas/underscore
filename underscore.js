a8adf4a04c9a41b9b6132cf1f1c63099143e2e8f
Kevin Malakoff <xmann.intl@gmail.com>
2011-09-22 07:26:22 +0200

    _.isEqual proposal: allow for _.isEqual to use the isEqual method on an object if it exists even if the object is a different type. I used this for comparing a Javascript string with a special localized string that contained an identifer in addition to the string. This is generally useful if values are wrapped in classes.

--- underscore.js
+++ underscore.js
@@ -609,19 +609,21 @@
   _.isEqual = function(a, b) {
     // Check object identity.
     if (a === b) return true;
+    // One is falsy and the other truthy.
+    if ((!a && b) || (a && !b)) return false;
+    // Either one is undefined
+    if ((a === void 0) || (b === void 0)) return false;
+    // One of them implements an isEqual()?
+    if (a.isEqual) return a.isEqual(b);
+    if (b.isEqual) return b.isEqual(a);
     // Different types?
     var atype = typeof(a), btype = typeof(b);
     if (atype != btype) return false;
     // Basic equality test (watch out for coercions).
     if (a == b) return true;
-    // One is falsy and the other truthy.
-    if ((!a && b) || (a && !b)) return false;
     // Unwrap any wrapped objects.
     if (a._chain) a = a._wrapped;
     if (b._chain) b = b._wrapped;
-    // One of them implements an isEqual()?
-    if (a.isEqual) return a.isEqual(b);
-    if (b.isEqual) return b.isEqual(a);
     // Check dates' integer values.
     if (_.isDate(a) && _.isDate(b)) return a.getTime() === b.getTime();
     // Both are NaN?
