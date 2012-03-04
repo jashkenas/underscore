6cd15681759bcee8b8c35be932ed7a04e970dd84
Matt Campbell <matt@mattcampbell.net>
2012-03-04 17:48:21 -0500

    Allow return-value of tap() to replace the current value

--- underscore.js
+++ underscore.js
@@ -665,8 +665,13 @@
   // The primary purpose of this method is to "tap into" a method chain, in
   // order to perform operations on intermediate results within the chain.
   _.tap = function(obj, interceptor) {
-    interceptor(obj);
-    return obj;
+    var result = interceptor(obj);
+
+    if (typeof result === 'undefined') {
+      return obj;
+    }
+
+    return result;
   };
 
   // Internal recursive comparison function.
