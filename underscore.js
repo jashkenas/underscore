fb75a118b06f4fc80de0d4c142fb0ddcfaea6306
Colin Thomas-Arnold <colinta@mac.com>
2012-04-16 13:27:21 -0600

    Don't escape `null`, just return `''`.

--- underscore.js
+++ underscore.js
@@ -884,6 +884,8 @@
 
   // Escape a string for HTML interpolation.
   _.escape = function(string) {
+    if ( string === null )
+      return '';
     return (''+string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;').replace(/\//g,'&#x2F;');
   };
 
