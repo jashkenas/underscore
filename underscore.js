47e7dbbd35be1439373539455b39c8858f5108ed
Jeremy Ashkenas <jashkenas@gmail.com>
2012-05-23 13:01:09 -0400

    removing needless isSorted optimization.

--- underscore.js
+++ underscore.js
@@ -384,8 +384,6 @@
   _.uniq = _.unique = function(array, isSorted, iterator) {
     var initial = iterator ? _.map(array, iterator) : array;
     var results = [];
-    // The `isSorted` flag is irrelevant if the array only contains two elements.
-    if (array.length < 3) isSorted = true;
     _.reduce(initial, function(memo, value, index) {
       if (isSorted ? _.last(memo) !== value || !memo.length : !_.include(memo, value)) {
         memo.push(value);
