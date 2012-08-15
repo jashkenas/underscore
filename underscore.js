3032ea278b2a6af85c9e044e6fd89dcd15af1751
Jeremy Ashkenas <jashkenas@gmail.com>
2012-08-15 12:31:47 -0400

    clearer 'variable' example for _.template, plus passive voice exorcism.

--- underscore.js
+++ underscore.js
@@ -1,6 +1,6 @@
 //     Underscore.js 1.3.3
 //     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
-//     Underscore is freely distributable under the MIT license.
+//     Underscore may be freely distributed under the MIT license.
 //     Portions of Underscore are inspired or borrowed from Prototype,
 //     Oliver Steele's Functional, and John Resig's Micro-Templating.
 //     For all details and documentation:
