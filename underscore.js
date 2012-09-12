8f8e988a9a04b08643735ef9e8b12445d18723b0
Jeremy Ashkenas <jashkenas@gmail.com>
2012-09-12 10:46:57 -0400

    Tweaking / cleaning implementation of _.uniq.

--- underscore.js
+++ underscore.js
@@ -416,13 +416,13 @@
   _.uniq = _.unique = function(array, isSorted, iterator) {
     var initial = iterator ? _.map(array, iterator) : array;
     var results = [];
-    _.reduce(initial, function(memo, value, index) {
-      if (isSorted ? (_.last(memo) !== value || !memo.length) : !_.include(memo, value)) {
-        memo.push(value);
+    var seen = [];
+    each(initial, function(value, index) {
+      if (isSorted ? (!index || seen[seen.length - 1] !== value) : !_.include(seen, value)) {
+        seen.push(value);
         results.push(array[index]);
       }
-      return memo;
-    }, []);
+    });
     return results;
   };
 
