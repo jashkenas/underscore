f869b6d18c372288e1287d111b7b923023d99e13
Jake Sandlund <jtsandlund@gmail.com>
2012-10-05 22:28:22 -0500

    Move oop to index.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 //     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
 //     Underscore may be freely distributed under the MIT license.
 
-var objects = require('./objects');
+var objects = require('./objects'),
+    utils = require('./utils');
 
 // Create a safe reference to the Underscore object for use below.
 var _ = function(obj) {
@@ -18,21 +19,65 @@ objects.extend(_,
                require('./collections'),
                require('./arrays'),
                require('./functions'),
-               require('./utils'),
-               require('./oop')
+               utils
               );
+
 
 // Add all of the Underscore functions to the wrapper object.
 _.mixin(_);
+
+var each = _.each,
+    ArrayProto = Array.prototype;
+
+// If Underscore is called as a function, it returns a wrapped object that
+// can be used OO-style. This wrapper holds altered versions of all the
+// underscore functions. Wrapped objects may be chained.
+
+var result = utils.createResult(_);
+
+// Add all mutator Array functions to the wrapper.
+each(['pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift'], function(name) {
+  var method = ArrayProto[name];
+  _.prototype[name] = function() {
+    var obj = this._wrapped;
+    method.apply(obj, arguments);
+    if ((name == 'shift' || name == 'splice') && obj.length === 0) delete obj[0];
+    return result.call(this, obj);
+  };
+});
+
+// Add all accessor Array functions to the wrapper.
+each(['concat', 'join', 'slice'], function(name) {
+  var method = ArrayProto[name];
+  _.prototype[name] = function() {
+    return result.call(this, method.apply(this._wrapped, arguments));
+  };
+});
+
+_.extend(_.prototype, {
+
+  // Start chaining a wrapped Underscore object.
+  chain: function() {
+    this._chain = true;
+    return this;
+  },
+
+  // Extracts the result from a wrapped and chained object.
+  value: function() {
+    return this._wrapped;
+  }
+
+});
+
 
 // Export the Underscore object for **Node.js**, with
 // backwards-compatibility for the old `require()` API. If we're in
 // the browser, add `_` as a global object via a string identifier,
 // for Closure Compiler "advanced" mode.
-if (typeof window === 'undefined') {
-  exports = module.exports = _;
-  exports._ = _;
-} else {
+module.exports = _;
+_._ = _;
+
+if (typeof window !== 'undefined') {
   window['_'] = _;
 }
 
