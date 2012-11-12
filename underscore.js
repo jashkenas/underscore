9404ac9597e1367c619b1e03c7780192146cdd73
Jeremy Ashkenas <jashkenas@gmail.com>
2012-11-11 21:57:14 -0500

    Fixes #856 -- use local var for arguments length check.

--- underscore.js
+++ underscore.js
@@ -130,7 +130,7 @@
     if (obj == null) obj = [];
     if (nativeReduceRight && obj.reduceRight === nativeReduceRight) {
       if (context) iterator = _.bind(iterator, context);
-      return arguments.length > 2 ? obj.reduceRight(iterator, memo) : obj.reduceRight(iterator);
+      return initial ? obj.reduceRight(iterator, memo) : obj.reduceRight(iterator);
     }
     var length = obj.length;
     if (length !== +length) {
