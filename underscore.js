c42a3e6772f36ee3b6372ce5b0b98c9fbdda158a
Jeremy Ashkenas <jashkenas@gmail.com>
2012-11-11 22:15:41 -0500

    Fixes #838 -- use hasOwnProperty when looping through _.where attrs, for healthy paranoia's sake

--- underscore.js
+++ underscore.js
@@ -239,7 +239,7 @@
     if (_.isEmpty(attrs)) return [];
     return _.filter(obj, function(value) {
       for (var key in attrs) {
-        if (attrs[key] !== value[key]) return false;
+        if (_.has(attrs, key) && attrs[key] !== value[key]) return false;
       }
       return true;
     });
