741360663e3033027860863db91a6e48a9f01501
Jeremy Ashkenas <jashkenas@gmail.com>
2012-12-11 12:12:42 -0500

    Revert "Fixes #891 -- pull native functions off of literals instead of prototypes."
    This reverts commit 15425de241d4021a5d6bc2c2a11a51a677060a42.

--- underscore.js
+++ underscore.js
@@ -17,31 +17,31 @@
   // Establish the object that gets returned to break out of a loop iteration.
   var breaker = {};
 
-  // Save bytes in the minified version:
-  var array = [], object = {};
+  // Save bytes in the minified (but not gzipped) version:
+  var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
 
   // Create quick reference variables for speed access to core prototypes.
-  var push             = array.push,
-      slice            = array.slice,
-      concat           = array.concat,
-      toString         = object.toString,
-      hasOwnProperty   = object.hasOwnProperty;
+  var push             = ArrayProto.push,
+      slice            = ArrayProto.slice,
+      concat           = ArrayProto.concat,
+      toString         = ObjProto.toString,
+      hasOwnProperty   = ObjProto.hasOwnProperty;
 
   // All **ECMAScript 5** native function implementations that we hope to use
   // are declared here.
   var
-    nativeForEach      = array.forEach,
-    nativeMap          = array.map,
-    nativeReduce       = array.reduce,
-    nativeReduceRight  = array.reduceRight,
-    nativeFilter       = array.filter,
-    nativeEvery        = array.every,
-    nativeSome         = array.some,
-    nativeIndexOf      = array.indexOf,
-    nativeLastIndexOf  = array.lastIndexOf,
+    nativeForEach      = ArrayProto.forEach,
+    nativeMap          = ArrayProto.map,
+    nativeReduce       = ArrayProto.reduce,
+    nativeReduceRight  = ArrayProto.reduceRight,
+    nativeFilter       = ArrayProto.filter,
+    nativeEvery        = ArrayProto.every,
+    nativeSome         = ArrayProto.some,
+    nativeIndexOf      = ArrayProto.indexOf,
+    nativeLastIndexOf  = ArrayProto.lastIndexOf,
     nativeIsArray      = Array.isArray,
     nativeKeys         = Object.keys,
-    nativeBind         = Function.prototype.bind;
+    nativeBind         = FuncProto.bind;
 
   // Create a safe reference to the Underscore object for use below.
   var _ = function(obj) {
