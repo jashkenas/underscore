86dc6fde5670d845d9591c18bbb92a7c9d43ef83
Giancarlo Palavicini <kasthor@gmail.com>
2012-12-18 07:42:42 -0600

    replaced type equality on where .IN. functionality

--- underscore.js
+++ underscore.js
@@ -240,7 +240,7 @@
     if (_.isEmpty(attrs)) return [];
     return _.filter(obj, function(value) {
       for (var key in attrs) {
-        if (attrs[key] instanceof Array)
+        if (any(attrs[key]))
           return _.include( attrs[key], value[key] );
         else
           if (attrs[key] !== value[key]) return false;
