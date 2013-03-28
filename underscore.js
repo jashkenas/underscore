f8560b52ef9077d966228e85797211997268f1d3
Tony <tony.tang.1119@gmail.com>
2013-03-28 17:24:10 +0800

    Update underscore.js

--- underscore.js
+++ underscore.js
@@ -374,7 +374,7 @@
   // Return the number of elements in an object.
   _.size = function(obj) {
     if (obj == null) return 0;
-    return (obj.length === +obj.length) ? obj.length : _.keys(obj).length;
+    return _.isArray(obj) ? obj.length : _.keys(obj).length;
   };
 
   // Array Functions
