1398555dd775928718e66f7e7585c0a30973568f
Oleg Taranenko <olegtaranenko@gmail.com>
2013-04-28 21:09:05 +0200

    get possible to break the loop _.each()

--- underscore.js
+++ underscore.js
@@ -52,6 +52,9 @@
     this._wrapped = obj;
   };
 
+  // allow break the loop, is being used in _.each() callback
+  _.break = breaker;
+
   // Export the Underscore object for **Node.js**, with
   // backwards-compatibility for the old `require()` API. If we're in
   // the browser, add `_` as a global object via a string identifier,
