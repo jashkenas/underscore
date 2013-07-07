6e0aff817dbff4c6cf0866d03bb13aacadb96c36
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2013-07-07 21:25:08 +0530

    fixing typos in chunk implementation

--- underscore.js
+++ underscore.js
@@ -94,18 +94,18 @@
   //similar to each, but introduces interval between each execution
   //helps in avoiding browser freezing when running heavy process
   var chunk = _.chunk = function(obj, iterator, context, interval){
-    if(obj==null) return;
-    if(interval === undefined) interval = 100;
-    if(obj.length === +obj.length){
-        obj = obj.concat();
-        setTimeout(function(){
-          item = obj.shift();
-          process.call(context, item);
-          if(array.length > 0){
-            setTimeout(arguments.callee, interval);
-          }
-        }, interval)
-    }
+      if(obj==null) return;
+      if(interval === undefined) interval = 100;
+      if(obj.length === +obj.length){
+          obj = obj.concat();
+          setTimeout(function(){
+              var item = obj.shift();
+              iterator.call(context, item);
+              if(obj.length > 0){
+                  setTimeout(arguments.callee, interval);
+              }
+          }, interval)
+      }
   }
 
   // Return the results of applying the iterator to each element.
