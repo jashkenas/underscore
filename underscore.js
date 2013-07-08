80bae6060b01b81b9696c7e2f5254c67a3401b32
aj0strow <alexander.ostrow@gmail.com>
2013-07-08 12:36:04 -0400

    _.default allows lazy props by using _.result

--- underscore.js
+++ underscore.js
@@ -820,7 +820,7 @@
     each(slice.call(arguments, 1), function(source) {
       if (source) {
         for (var prop in source) {
-          if (obj[prop] === void 0) obj[prop] = source[prop];
+          if (obj[prop] === void 0) obj[prop] = _.result(source, prop);
         }
       }
     });
