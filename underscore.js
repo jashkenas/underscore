b3985b68844c9907989e10cc4f77d5c2ee452ae3
Brad Dunbar <dunbarb2@gmail.com>
2013-08-09 17:20:31 -0400

    Tweak tweak.

--- underscore.js
+++ underscore.js
@@ -306,21 +306,18 @@
     var iterator = lookupIterator(value);
     return _.pluck(_.map(obj, function(value, index, list) {
       return {
-        value : value,
-        index : index,
-        criteria : iterator.call(context, value, index, list)
+        value: value,
+        index: index,
+        criteria: iterator.call(context, value, index, list)
       };
     }).sort(function(left, right) {
-      if(left === right){
-        return 0;
-      }
       var a = left.criteria;
       var b = right.criteria;
       if (a !== b) {
         if (a > b || a === void 0) return 1;
         if (a < b || b === void 0) return -1;
       }
-      return left.index < right.index ? -1 : 1;
+      return left.index - right.index;
     }), 'value');
   };
 
