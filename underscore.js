ef17a76af6d15a140fb4ebeed9709530f8f4b2c4
Brad Dunbar <dunbarb2@gmail.com>
2013-09-07 15:06:53 -0400

    Use _.values for sampling objects.

--- underscore.js
+++ underscore.js
@@ -302,11 +302,8 @@
   // The internal `guard` argument allows it to work with `map`.
   _.sample = function(obj, n, guard) {
     if (n == null || guard) {
-      if (obj.length === +obj.length) {
-        return obj[_.random(obj.length - 1)];
-      } else {
-        return obj[_.sample(_.keys(obj))];
-      }
+      if (obj.length !== +obj.length) obj = _.values(obj);
+      return obj[_.random(obj.length - 1)];
     }
     return _.shuffle(obj).slice(0, Math.max(0, n));
   };
