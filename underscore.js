acfee5bb07a26827859125f86c0d95d495304f17
jpalomar <jr@macremefraiche.com>
2013-11-03 09:33:58 -0600

    Support js minifier license comment convention
    Many JS minifiers use the convention that comments starting with '/*!' are preserved as license comments. This ensures the
    license comment is preserved when using these tools.

--- underscore.js
+++ underscore.js
@@ -1,8 +1,9 @@
-//     Underscore.js 1.5.2
-//     http://underscorejs.org
-//     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
-//     Underscore may be freely distributed under the MIT license.
-
+/*!
+    Underscore.js 1.5.2
+    http://underscorejs.org
+    (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+    Underscore may be freely distributed under the MIT license.
+*/
 (function() {
 
   // Baseline setup
