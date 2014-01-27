8f5e0914a965ca67aad8c82db38777e2edbfecd7
Michael Ficarra <mficarra@groupon.com>
2014-01-27 13:54:54 -0600

    Revert "use _.partial in definition of _.intersection"
    This reverts commit 826f8ceba0699a8a09991015f19c8a10444c8076.

--- underscore.js
+++ underscore.js
@@ -492,7 +492,9 @@
   _.intersection = function(array) {
     var rest = slice.call(arguments, 1);
     return _.filter(_.uniq(array), function(item) {
-      return _.every(rest, _.partial(_.contains, _, item));
+      return _.every(rest, function(other) {
+        return _.contains(other, item);
+      });
     });
   };
 
