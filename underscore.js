1a13e2ee984cb14225852d3574db013590e792a2
Brad Dunbar <dunbarb2@gmail.com>
2014-03-28 11:20:36 -0400

    Edit for style.

--- underscore.js
+++ underscore.js
@@ -518,16 +518,15 @@
   // passed-in arrays.
   _.intersection = function(array) {
     if (array == null) return [];
-    var argsLength = arguments.length,
-        result = [];
+    var result = [];
+    var argsLength = arguments.length;
     for (var i = 0, length = array.length; i < length; i++) {
       var item = array[i];
-      if (!_.contains(result, item)) {
-        for (var j = 1; j < argsLength; j++) {
-          if (!_.contains(arguments[j], item)) break;
-        }
-        if (j === argsLength) result.push(item);
+      if (_.contains(result, item)) continue;
+      for (var j = 1; j < argsLength; j++) {
+        if (!_.contains(arguments[j], item)) break;
       }
+      if (j === argsLength) result.push(item);
     }
     return result;
   };
