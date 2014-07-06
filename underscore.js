8e45ea5fa09ea320c5016b83c3e623859de2e832
Andrew Ivaskevych <AndrewEastwood@users.noreply.github.com>
2014-07-06 13:04:40 +0300

    Update underscore.js
    Added condition to check whether obj is number and equals to 0.
    In other words the function should return true in the following case: ```_.isEmpty(0)```

--- underscore.js
+++ underscore.js
@@ -1073,6 +1073,7 @@
   _.isEmpty = function(obj) {
     if (obj == null) return true;
     if (_.isArray(obj) || _.isString(obj) || _.isArguments(obj)) return obj.length === 0;
+    if (_.isNumber(obj)) return obj === 0;
     for (var key in obj) if (_.has(obj, key)) return false;
     return true;
   };
