55b9674163e5794eed786a90d6a5cea90411a205
Graeme Yeates <gyeates@clearpathrobotics.com>
2014-07-10 10:11:51 -0400

    shuffle: Avoid swapping current index

--- underscore.js
+++ underscore.js
@@ -311,7 +311,7 @@
     var shuffled = Array(length);
     for (var index = 0, rand; index < length; index++) {
       rand = _.random(0, index);
-      shuffled[index] = shuffled[rand];
+      if (rand !== index) shuffled[index] = shuffled[rand];
       shuffled[rand] = set[index];
     }
     return shuffled;
