d858d715dea7dced68baad6f4a53bf79af94bd32
Hack Reactor Students <accounts@hackreactor.com>
2014-08-02 15:53:34 -0700

    Simplify variable assignments for _.every

--- underscore.js
+++ underscore.js
@@ -200,12 +200,9 @@
   _.every = _.all = function(obj, predicate, context) {
     if (obj == null) return true;
     predicate = _.iteratee(predicate, context);
-    var length = obj.length;
-    var index, currentKey, keys;
-    if (length !== +length) {
-      keys = _.keys(obj);
-      length = keys.length;
-    }
+    var keys = obj.length !== +obj.length && _.keys(obj),
+        length = (keys || obj).length,
+        index, currentKey;
     for (index = 0; index < length; index++) {
       currentKey = keys ? keys[index] : index;
       if (!predicate(obj[currentKey], currentKey, obj)) return false;
