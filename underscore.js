1b3c37ee0affe197f09fe02ae5f2fab2a8f2e5e7
Hack Reactor Students <accounts@hackreactor.com>
2014-08-02 16:11:25 -0700

    Simplify _.each by removing extra variable declarations

--- underscore.js
+++ underscore.js
@@ -97,14 +97,13 @@
   _.each = _.forEach = function(obj, iteratee, context) {
     if (obj == null) return obj;
     iteratee = createCallback(iteratee, context);
-    var i, length = obj.length;
-    if (length === +length) {
-      for (i = 0; i < length; i++) {
+    if (obj.length === +obj.length) {
+      for (var i = 0; i < obj.length; i++) {
         iteratee(obj[i], i, obj);
       }
     } else {
       var keys = _.keys(obj);
-      for (i = 0, length = keys.length; i < length; i++) {
+      for (var i = 0; i < keys.length; i++) {
         iteratee(obj[keys[i]], keys[i], obj);
       }
     }
