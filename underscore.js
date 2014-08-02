557fcf6be9b110db7b3df0c92a4c60906c87d8ff
Hack Reactor Students <accounts@hackreactor.com>
2014-08-02 15:57:15 -0700

    Simplify variable assignments for _.some

--- underscore.js
+++ underscore.js
@@ -215,12 +215,9 @@
   _.some = _.any = function(obj, predicate, context) {
     if (obj == null) return false;
     predicate = _.iteratee(predicate, context);
-    var length = obj.length;
-    var index, currentKey, keys;
-    if (length !== +length) {
-      keys = _.keys(obj);
-      length = keys.length;
-    }
+    var keys = obj.length !== +obj.length && _.keys(obj),
+        length = (keys || obj).length,
+        index, currentKey;
     for (index = 0; index < length; index++) {
       currentKey = keys ? keys[index] : index;
       if (predicate(obj[currentKey], currentKey, obj)) return true;
