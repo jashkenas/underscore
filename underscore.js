decfb9d938cf976798d794d3dcccfa8c17662803
Adam Dymowski <adam.dymowski@bluemedia.pl>
2014-08-08 14:02:10 +0200

    accented chars support

--- underscore.js
+++ underscore.js
@@ -344,13 +344,8 @@
         criteria: iteratee(value, index, list)
       };
     }).sort(function(left, right) {
-      var a = left.criteria;
-      var b = right.criteria;
-      if (a !== b) {
-        if (a > b || a === void 0) return 1;
-        if (a < b || b === void 0) return -1;
-      }
-      return left.index - right.index;
+      var a = left.criteria, b = right.criteria;
+      return a.localeCompare(b);
     }), 'value');
   };
 
