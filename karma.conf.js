599a2064bd11135db184737c771137ff344c8065
Graeme Yeates <gyeates@clearpathrobotics.com>
2014-09-03 14:31:50 -0400

    Set up testing via Karma

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['qunit'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'underscore.js',
+      'test/*.js'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
