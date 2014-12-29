644b35b1328d5bd633578378f4231a734f0bdf90
Graeme <megawac@gmail.com>
2014-12-29 13:01:50 -0500

    Invoke shouldn't throw for null or undefined

--- underscore.js
+++ underscore.js
@@ -291,7 +291,8 @@
     var args = slice.call(arguments, 2);
     var isFunc = _.isFunction(method);
     return _.map(obj, function(value) {
-      return (isFunc ? method : value[method]).apply(value, args);
+      var func = isFunc ? method : value[method];
+      return func == null ? func : func.apply(value, args);
     });
   };
 
