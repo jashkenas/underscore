439144dd467a4af3cb1cd12e137e37caaa08d882
BishopZ <bishop@bloomfire.com>
2015-01-08 14:33:02 -0600

    PR for issue #1996, enhance _.bindAll

--- underscore.js
+++ underscore.js
@@ -749,8 +749,11 @@
     var i, length = arguments.length, key;
     if (length <= 1) throw new Error('bindAll must be passed function names');
     for (i = 1; i < length; i++) {
-      key = arguments[i];
-      obj[key] = _.bind(obj[key], obj);
+      keys = arguments[i];
+      if (!_.isArray(keys)) keys = [keys];
+      _.each(keys, function(key){
+        obj[key] = _.bind(obj[key], obj);
+      });
     }
     return obj;
   };
