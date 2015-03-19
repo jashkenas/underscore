67e28bbccff128594268883097020594ff98c2a8
Rolf Bjaanes <rolf@powow.no>
2015-03-19 14:28:46 +0100

    fix _.isEmpty for empty NodeList object in WebKit

--- underscore.js
+++ underscore.js
@@ -1188,7 +1188,7 @@
   // An "empty" object has no enumerable own-properties.
   _.isEmpty = function(obj) {
     if (obj == null) return true;
-    if (isArrayLike(obj) && (_.isArray(obj) || _.isString(obj) || _.isArguments(obj))) return obj.length === 0;
+    if (isArrayLike(obj) && (_.isArray(obj) || _.isString(obj) || _.isArguments(obj) || _.has(obj, 'length'))) return obj.length === 0;
     return _.keys(obj).length === 0;
   };
 
