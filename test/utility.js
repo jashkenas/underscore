f4c4c58ef7293114021ebac8994ace870eaadc79
Matthew Baker <mbaker@kohesion.net>
2015-08-06 18:12:16 -0400

    smok'n crack
    PhantomJS wasn’t the problem, it was me :(

--- test/utility.js
+++ test/utility.js
@@ -53,7 +53,7 @@
 
   test('collection', function() {
     equal(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]).length, 4, 'should create collection of expected length');
-    propEqual(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]), [{name: 'Anastasia', score: 90}, {name: 'Suzanne', score: 95}, {name: 'Edmund', score: 100}, {score: 50}], 'should create expected object');
+    equal(_.isEqual(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]), [{name: 'Anastasia', score: 90}, {name: 'Suzanne', score: 95}, {name: 'Edmund', score: 100}, {name: void 0, score: 50}]), true, 'should create expected object');
   });
 
   test('identity', function() {
