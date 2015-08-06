e1bf107d0a62181a9b40a3353e8631437536c6b4
Matthew Baker <mbaker@kohesion.net>
2015-08-06 17:51:34 -0400

    Adding tests
    Not sure why tests are failing, trying some ideas out.

--- test/utility.js
+++ test/utility.js
@@ -52,7 +52,8 @@
   });
 
   test('collection', function() {
-    equal(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]), [{name: 'Anastasia', score: 90}, {name: 'Suzanne', score: 95}, {name: 'Edmund', score: 100}, {score: 50}]);
+    equal(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]).length, 4, 'should create collection of expected length');
+    deepEqual(_.collection(['name', 'score'], [['Anastasia', 'Suzanne', 'Edmund'], [90, 95, 100, 50]]), [{name: 'Anastasia', score: 90}, {name: 'Suzanne', score: 95}, {name: 'Edmund', score: 100}, {score: 50}], 'should create expected object');
   });
 
   test('identity', function() {
