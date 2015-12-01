a595fe10aa14f382efcac03a793270ef779c5b37
Justin Ridgewell <justin@ridgewell.name>
2015-11-30 22:59:42 -0500

    Negative Ranges
    Fixes https://github.com/jashkenas/underscore/issues/2364.

--- underscore.js
+++ underscore.js
@@ -693,7 +693,9 @@
       stop = start || 0;
       start = 0;
     }
-    step = step || 1;
+    if (!step) {
+      step = stop < start ? -1 : 1;
+    }
 
     var length = Math.max(Math.ceil((stop - start) / step), 0);
     var range = Array(length);
