6c2c83e191e366155dac7bdfbf8df37fd33e0a80
Rex Kelly <rexrkelly@MIFI-HUB-2.local>
2016-04-14 15:14:35 -0700

    [Fix] minor styling issues with my last commit

--- underscore.js
+++ underscore.js
@@ -1360,9 +1360,9 @@
   };
 
   // Simple wrapper for _.identity, for an alternate symantic meaning.
-  _.echo = function(value){
+  _.echo = function(value) {
     return _.identity(value);
-  }
+  };
 
   // Predicate-generating functions. Often useful outside of Underscore.
   _.constant = function(value) {
