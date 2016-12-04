f5bc46198fd9e3467834a8f8a2328cce62040d2c
QiuYao <41918235@qq.com>
2016-12-04 20:21:07 +0800

    Update underscore.js

--- underscore.js
+++ underscore.js
@@ -621,13 +621,11 @@
   // each array's elements on shared indices.
   _.unzip = function(array) {
     var length = array && _.max(array, getLength).length || 0;
-    var result = Array(length);
-
-    for (var index = 0; index < length; index++) {
-      result[index] = _.pluck(array, index);
-    }
-    return result;
+    return _.times(length, function(index) { 
+        return _.pluck(array, index);
+    });
   };
+
 
   // Zip together multiple lists into a single array -- elements that share
   // an index go together.
