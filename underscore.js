248a3a21470f91a657001fb27df9b368828d2fd2
ershing <ershing@foxmail.com>
2017-07-04 11:05:13 +0800

    Update underscore.js

--- underscore.js
+++ underscore.js
@@ -750,13 +750,15 @@
 
     objectArrayOne.forEach(function(eleOne) {
       objectArrayTwo.forEach(function(eleTwo) {
-        if (eleOne[elementOne] === eleTwo[secondEle] && eleOne[elementOne] !== undefined) {
-          var combinedObject = JSON.parse(JSON.stringify(eleOne));
-          for (var e2 in eleTwo) {
-            combinedObject[e2] = eleTwo[e2];
-          }
-          if (resArray.indexOf(combinedObject) === -1) {
-            resArray.push(combinedObject);
+        if (eleOne[elementOne] === eleTwo[secondEle]) {
+          if(eleOne[elementOne] !== undefined){
+            var combinedObject = JSON.parse(JSON.stringify(eleOne));
+            for (var e2 in eleTwo) {
+              combinedObject[e2] = eleTwo[e2];
+            }
+            if (resArray.indexOf(combinedObject) === -1) {
+              resArray.push(combinedObject);
+            }
           }
         }
       });
