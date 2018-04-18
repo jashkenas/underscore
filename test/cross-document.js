c9943aaf0d8042bbee86eb790ea6f6ab0e54b037
jashkenas <jashkenas@gmail.com>
2018-04-18 10:28:47 -0700

    Upgrading QUnit etc. Fixing Node tests.

--- test/cross-document.js
+++ test/cross-document.js
@@ -36,7 +36,7 @@
 
   QUnit.test('isEqual', function(assert) {
 
-    assert.notOk(_.isEqual(iNumber, 101));
+    assert.ok(!_.isEqual(iNumber, 101));
     assert.ok(_.isEqual(iNumber, 100));
 
     // Objects from another frame.
@@ -47,13 +47,13 @@
   });
 
   QUnit.test('isEmpty', function(assert) {
-    assert.notOk(_([iNumber]).isEmpty(), '[1] is not empty');
-    assert.notOk(_.isEmpty(iArray), '[] is empty');
+    assert.ok(!_([iNumber]).isEmpty(), '[1] is not empty');
+    assert.ok(!_.isEmpty(iArray), '[] is empty');
     assert.ok(_.isEmpty(iObject), '{} is empty');
   });
 
   QUnit.test('isElement', function(assert) {
-    assert.notOk(_.isElement('div'), 'strings are not dom elements');
+    assert.ok(!_.isElement('div'), 'strings are not dom elements');
     assert.ok(_.isElement(document.body), 'the body tag is a DOM element');
     assert.ok(_.isElement(iElement), 'even from another frame');
   });
@@ -114,12 +114,12 @@
   if (typeof ActiveXObject != 'undefined') {
     QUnit.test('IE host objects', function(assert) {
       var xml = new ActiveXObject('Msxml2.DOMDocument.3.0');
-      assert.notOk(_.isNumber(xml));
-      assert.notOk(_.isBoolean(xml));
-      assert.notOk(_.isNaN(xml));
-      assert.notOk(_.isFunction(xml));
-      assert.notOk(_.isNull(xml));
-      assert.notOk(_.isUndefined(xml));
+      assert.ok(!_.isNumber(xml));
+      assert.ok(!_.isBoolean(xml));
+      assert.ok(!_.isNaN(xml));
+      assert.ok(!_.isFunction(xml));
+      assert.ok(!_.isNull(xml));
+      assert.ok(!_.isUndefined(xml));
     });
 
     QUnit.test('#1621 IE 11 compat mode DOM elements are not functions', function(assert) {
