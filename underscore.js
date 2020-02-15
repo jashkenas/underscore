e7afaa395646475e64fa86bfec98f55f0e0e6d9f
Kranite <45276438+Kranite@users.noreply.github.com>
2020-02-15 19:03:28 +0800

    Update underscore.js

--- underscore.js
+++ underscore.js
@@ -295,7 +295,7 @@
         length = (keys || obj).length;
       for (var index = 0; index < length; index++) {
         var currentKey = keys ? keys[index] : index;
-        if (predicate(obj[currentKey], currentKey, obj) == result) return result;
+        if (!!predicate(obj[currentKey], currentKey, obj) == result) return result;
       }
       return !result;
     };
