e1a5518a144b5431f7ad74e3bcf32e0ed6c4fe78
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-03-30 01:41:19 +0200

    Bump the version number in the documentation and source code

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,4 @@
-//     Underscore.js 1.9.2
+//     Underscore.js 1.10.0
 //     https://underscorejs.org
 //     (c) 2009-2018 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
@@ -46,7 +46,7 @@ export default function _(obj) {
 }
 
 // Current version.
-export var VERSION = _.VERSION = '1.9.2';
+export var VERSION = _.VERSION = '1.10.0';
 
 // Internal function that returns an efficient (for current engines) version
 // of the passed-in callback, to be repeatedly applied in other Underscore
