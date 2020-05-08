ed8d5b136773bd0a26cac5a5c3fceaa332f187a0
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-05-08 11:31:53 +0200

    Create modular AMD and CommonJS builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,63 @@
-var fs = require('fs');
+import { readFileSync } from 'fs';
+import _ from './underscore-esm.js';
+import glob from 'glob';
 
-var intro = fs.readFileSync('modules/index.js', 'utf-8').split('\n').slice(0, 4).join('\n');
+var intro = readFileSync('modules/index.js', 'utf-8').split('\n').slice(0, 4).join('\n');
 
-module.exports = [{
-  input: 'modules/index-all.js',
-  treeshake: false,
-  output: {
-    file: 'underscore-esm.js',
-    intro,
-    format: 'esm',
-    sourcemap: true,
-    sourcemapExcludeSources: true,
+var outputBase = {
+  strict: false,
+  externalLiveBindings: false,
+  freeze: false,
+};
+
+export default [
+  // Monolithic ESM bundle for client use.
+  {
+    input: 'modules/index-all.js',
+    treeshake: false,
+    output: _.extend({
+      file: 'underscore-esm.js',
+      intro,
+      format: 'esm',
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    }, outputBase),
   },
-}, {
-  input: 'modules/index-default.js',
-  treeshake: false,
-  output: {
-    file: 'underscore.js',
-    exports: 'default',
-    intro,
-    format: 'umd',
-    name: '_',
-    amd: {
-      id: 'underscore',
-    },
-    noConflict: true,
-    sourcemap: true,
-    sourcemapExcludeSources: true,
-    legacy: true,
-    strict: false,
-    externalLiveBindings: false,
-    freeze: false,
+  // Monolithic UMD bundle for client use.
+  {
+    input: 'modules/index-default.js',
+    treeshake: false,
+    output: _.extend({
+      file: 'underscore.js',
+      exports: 'default',
+      intro,
+      format: 'umd',
+      name: '_',
+      amd: {
+        id: 'underscore',
+      },
+      noConflict: true,
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    }, outputBase),
   },
-}];
+  // AMD and CJS versions of the individual modules for development, server use
+  // and custom bundles.
+  {
+    input: _.filter(
+      glob.sync('modules/**/*.js'),
+      function(path) { return path !== 'modules/index-all.js'; }
+    ),
+    preserveModules: true,
+    output: [
+      _.extend({
+        dir: 'amd',
+        format: 'amd',
+      }, outputBase),
+      _.extend({
+        dir: 'cjs',
+        format: 'cjs',
+      }, outputBase),
+    ],
+  }
+];
