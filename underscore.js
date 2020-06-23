6caad89bac6ab6c3dc9ef98e6f36792776d95e08
Ben Carp <carpben@users.noreply.github.com>
2020-06-23 09:45:03 +0300

    Updating debounce documentation

--- underscore.js
+++ underscore.js
@@ -891,10 +891,12 @@
     return throttled;
   }
 
-  // Returns a function, that, as long as it continues to be invoked, will not
-  // be triggered. The function will be called after it stops being called for
-  // N milliseconds. If `immediate` is passed, trigger the function on the
-  // leading edge, instead of the trailing.
+  
+  
+  // When a sequence of calls of the returned function ends, the argument
+  // function is triggered. The end of a sequence is defined by the ‘wait’ 
+  // parameter. If ‘immediate’ is passed the argument function would be 
+  // triggered at the beginning of the sequence as well. 
   function debounce(func, wait, immediate) {
     var timeout, result;
 
