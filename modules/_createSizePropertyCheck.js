53c3809dcfadc27091c20a0ea634f34254c3506b
hongliang.yu01 <hongliang.yu01@liulishuo.com>
2020-06-30 15:23:36 +0800

    fix: isarraylike except function type

--- modules/_createSizePropertyCheck.js
+++ modules/_createSizePropertyCheck.js
@@ -1,9 +1,15 @@
-import { MAX_ARRAY_INDEX } from './_setup.js';
+import { MAX_ARRAY_INDEX } from "./_setup.js";
+import isFunction from "./isFunction";
 
 // Common internal logic for `isArrayLike` and `isBufferLike`.
 export default function createSizePropertyCheck(getSizeProperty) {
-  return function(collection) {
+  return function (collection) {
     var sizeProperty = getSizeProperty(collection);
-    return typeof sizeProperty == 'number' && sizeProperty >= 0 && sizeProperty <= MAX_ARRAY_INDEX;
-  }
+    return (
+      !isFunction(collection) &&
+      typeof sizeProperty == "number" &&
+      sizeProperty >= 0 &&
+      sizeProperty <= MAX_ARRAY_INDEX
+    );
+  };
 }
