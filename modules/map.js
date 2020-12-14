ea55aeb03e8659f272e7341124d71949ae03de63
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-12-14 15:01:20 +0100

    Use _.each in _.map

--- modules/map.js
+++ modules/map.js
@@ -1,16 +1,12 @@
 import cb from './_cb.js';
-import isArrayLike from './_isArrayLike.js';
-import keys from './keys.js';
+import each from './each.js';
 
 // Return the results of applying the iteratee to each element.
 export default function map(obj, iteratee, context) {
   iteratee = cb(iteratee, context);
-  var _keys = !isArrayLike(obj) && keys(obj),
-      length = (_keys || obj).length,
-      results = Array(length);
-  for (var index = 0; index < length; index++) {
-    var currentKey = _keys ? _keys[index] : index;
-    results[index] = iteratee(obj[currentKey], currentKey, obj);
-  }
+  var results = [];
+  each(obj, function(value, key) {
+    results.push(iteratee(value, key, obj));
+  });
   return results;
 }
--- underscore-esm.js
+++ underscore-esm.js
@@ -1290,13 +1290,10 @@ function each(obj, iteratee, context) {
 // Return the results of applying the iteratee to each element.
 function map(obj, iteratee, context) {
   iteratee = cb(iteratee, context);
-  var _keys = !isArrayLike(obj) && keys(obj),
-      length = (_keys || obj).length,
-      results = Array(length);
-  for (var index = 0; index < length; index++) {
-    var currentKey = _keys ? _keys[index] : index;
-    results[index] = iteratee(obj[currentKey], currentKey, obj);
-  }
+  var results = [];
+  each(obj, function(value, key) {
+    results.push(iteratee(value, key, obj));
+  });
   return results;
 }
 
