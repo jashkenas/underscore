cdce73f9c7998374c2488aa0cf4df719bd1baa09
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2020-12-17 22:47:06 +0330

    toArray function now supports iterable objects

---
+++ modules/iteratorToArray.js
@@ -0,0 +1,9 @@
+export default function iteratorToArray(iterator) {
+  var data,
+      result = [];
+
+  while (!(data = iterator.next()).done) {
+    result.push(data.value);
+  }
+  return result;
+}
--- modules/toArray.js
+++ modules/toArray.js
@@ -5,7 +5,8 @@ import isArrayLike from './_isArrayLike.js';
 import map from './map.js';
 import identity from './identity.js';
 import values from './values.js';
-
+import isIterable from './isIterable.js'
+import iteratorToArray from './iteratorToArray'
 // Safely create a real, live array from anything iterable.
 var reStrSymbol = /[^\ud800-\udfff]|[\ud800-\udbff][\udc00-\udfff]|[\ud800-\udfff]/g;
 export default function toArray(obj) {
@@ -16,5 +17,6 @@ export default function toArray(obj) {
     return obj.match(reStrSymbol);
   }
   if (isArrayLike(obj)) return map(obj, identity);
+  else if (isIterable(obj)) return iteratorToArray(obj[Symbol.iterator])
   return values(obj);
 }
