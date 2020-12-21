296be9efd73bcd1ae0cc286f840dcc3349fda4d5
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-12-21 21:47:25 +0100

    Remove switch pyramid of doom from _.restArguments

--- modules/restArguments.js
+++ modules/restArguments.js
@@ -12,11 +12,6 @@ export default function restArguments(func, startIndex) {
     for (; index < length; index++) {
       rest[index] = arguments[index + startIndex];
     }
-    switch (startIndex) {
-      case 0: return func.call(this, rest);
-      case 1: return func.call(this, arguments[0], rest);
-      case 2: return func.call(this, arguments[0], arguments[1], rest);
-    }
     var args = Array(startIndex + 1);
     for (index = 0; index < startIndex; index++) {
       args[index] = arguments[index];
