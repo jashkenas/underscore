5781d89c9a462aae5083aac22b8cd7efee21e33d
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-12-30 02:41:33 +0100

    Use array.push in _.partial

--- underscore.js
+++ underscore.js
@@ -954,9 +954,9 @@
     var placeholder = partial.placeholder;
     var bound = function() {
       var position = 0, length = boundArgs.length;
-      var args = Array(length);
+      var args = [];
       for (var i = 0; i < length; i++) {
-        args[i] = boundArgs[i] === placeholder ? arguments[position++] : boundArgs[i];
+        args.push(boundArgs[i] === placeholder ? arguments[position++] : boundArgs[i]);
       }
       while (position < arguments.length) args.push(arguments[position++]);
       return executeBound(func, bound, this, this, args);
