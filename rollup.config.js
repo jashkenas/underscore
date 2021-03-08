4fc14f117dd35f3383665d3ab65530394a4282f8
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-03-08 03:25:38 +0100

    Refactor the rollup config a bit

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-import _ from './underscore-esm.js';
+import { extend, filter } from './underscore-esm.js';
 import glob from 'glob';
 
 var intro = readFileSync('modules/index.js', 'utf-8').split('\n').slice(3, 7).join('\n');
@@ -10,54 +10,62 @@ var outputBase = {
   freeze: false,
 };
 
+var monolithicBase = {
+  intro,
+  sourcemap: true,
+  sourcemapExcludeSources: true,
+};
+
+function outputConf(particular) {
+  return extend(particular, outputBase);
+}
+
+function monolithConf(particular) {
+  return extend(particular, outputBase, monolithicBase);
+}
+
 export default [
   // Monolithic ESM bundle for client use.
   {
     input: 'modules/index-all.js',
     treeshake: false,
-    output: _.extend({
+    output: monolithConf({
       file: 'underscore-esm.js',
-      intro,
       format: 'esm',
-      sourcemap: true,
-      sourcemapExcludeSources: true,
-    }, outputBase),
+    }),
   },
   // Monolithic UMD bundle for client use.
   {
     input: 'modules/index-default.js',
     treeshake: false,
-    output: _.extend({
+    output: monolithConf({
       file: 'underscore.js',
       exports: 'default',
-      intro,
       format: 'umd',
       name: '_',
       amd: {
         id: 'underscore',
       },
       noConflict: true,
-      sourcemap: true,
-      sourcemapExcludeSources: true,
-    }, outputBase),
+    }),
   },
   // AMD and CJS versions of the individual modules for development, server use
   // and custom bundles.
   {
-    input: _.filter(
+    input: filter(
       glob.sync('modules/**/*.js'),
       function(path) { return path !== 'modules/index-all.js'; }
     ),
     preserveModules: true,
     output: [
-      _.extend({
+      outputConf({
         dir: 'amd',
         format: 'amd',
-      }, outputBase),
-      _.extend({
+      }),
+      outputConf({
         dir: 'cjs',
         format: 'cjs',
-      }, outputBase),
+      }),
     ],
   }
 ];
