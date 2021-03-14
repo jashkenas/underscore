87b5ecdf5b33335865d34f1d88f71b04473b647a
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-03-14 20:37:42 +0100

    Bump the version to 1.13.0-2

--- underscore-node.cjs
+++ underscore-node.cjs
@@ -1,4 +1,4 @@
-//     Underscore.js 1.13.0-1
+//     Underscore.js 1.13.0-2
 //     https://underscorejs.org
 //     (c) 2009-2020 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
