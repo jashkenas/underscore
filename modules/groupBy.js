1ae4adca1bfd8e742a6ee9c94095f94876dc5e18
Félix Andersson <56377864+Felichz@users.noreply.github.com>
2021-09-23 16:40:04 -0300

    Update groupBy.js

--- modules/groupBy.js
+++ modules/groupBy.js
@@ -3,6 +3,6 @@ import has from './_has.js';
 
 // Groups the object's values by a criterion. Pass either a string attribute
 // to group by, or a function that returns the criterion.
-export default group(function(result, value, key) {
-  if (has(result, key)) result[key].push(value); else result[key] = [value];
+export default group((result, value, key) => {
+  result[key] = [...(result[key] || []), value];
 });
