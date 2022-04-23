1022ab3719b11c4cad33b301e3dbef4369354880
Julian Gonggrijp <dev@juliangonggrijp.com>
2022-04-23 19:01:41 +0200

    Update the LICENSE date

--- modules/index.js
+++ modules/index.js
@@ -3,7 +3,7 @@
 
 //     Underscore.js 1.13.3
 //     https://underscorejs.org
-//     (c) 2009-2021 Jeremy Ashkenas, Julian Gonggrijp, and DocumentCloud and Investigative Reporters & Editors
+//     (c) 2009-2022 Jeremy Ashkenas, Julian Gonggrijp, and DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
 
 // Baseline setup.
--- underscore-node-f.cjs
+++ underscore-node-f.cjs
@@ -1,6 +1,6 @@
 //     Underscore.js 1.13.3
 //     https://underscorejs.org
-//     (c) 2009-2021 Jeremy Ashkenas, Julian Gonggrijp, and DocumentCloud and Investigative Reporters & Editors
+//     (c) 2009-2022 Jeremy Ashkenas, Julian Gonggrijp, and DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
 
 Object.defineProperty(exports, '__esModule', { value: true });
