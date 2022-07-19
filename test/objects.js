2e38b16112b34a16456962e9421cce5ea695efc5
Azat <example101110@gmail.com>
2022-07-19 15:47:23 +0500

    added tests for `set`

--- test/objects.js
+++ test/objects.js
@@ -1255,4 +1255,29 @@
     assert.deepEqual(_.mapObject(protoObj, _.identity), {a: 1}, 'ignore inherited values from prototypes');
 
   });
+
+
+  QUnit.test('set', function(assert) {
+    // returns first argument if first argument is primitive type
+    assert.strictEqual(_.set(undefined, ['some', 'path'], 'some value'), undefined);
+    assert.strictEqual(_.set(null, ['some', 'path'], 'some value'), null);
+    assert.strictEqual(_.set(12, ['some', 'path'], 'some value'), 12);
+    assert.strictEqual(_.set(BigInt(1), ['some', 'path'], 'some value'), BigInt(1));
+    assert.strictEqual(_.set('string', ['some', 'path'], 'some value'), 'string');
+    assert.strictEqual(_.set(true, ['some', 'path'], 'some value'), true);
+
+    // returns the same object if path is not an array or its empty
+    assert.deepEqual(_.set({random: {object: true}}, undefined, 'some value'), {random: {object: true}});
+    assert.deepEqual(_.set({random: {object: true}}, {}, 'some value'), {random: {object: true}});
+    assert.deepEqual(_.set({random: {object: true}}, [], 'some value'), {random: {object: true}});
+
+    assert.deepEqual(_.set({x: {l: 10}}, ['x', 'l'], 'changed'), {x: {l: 'changed'}});
+    assert.deepEqual(_.set({x: 10}, ['my', 'path'], 'my value'), {x: 10, my: {path: 'my value'}});
+    assert.deepEqual(_.set({x: 10}, ['my', 0, 'path'], 'my value'), {x: 10, my: [{path: 'my value'}]});
+    assert.deepEqual(_.set({x: 10}, [0], 'my value'), {x: 10, '0': 'my value'});
+    assert.deepEqual(_.set({x: 10}, [0, 1], 'my value'), {x: 10, '0': [undefined, 'my value']});
+    assert.deepEqual(_.set({x: 10}, [0, 0], 'my value'), {x: 10, '0': ['my value']});
+    assert.deepEqual(_.set({x: 10}, [0, '0'], 'my value'), {x: 10, '0': {'0': 'my value'}});
+    assert.deepEqual(_.set({x: 10}, [0, 'my'], 'my value'), {x: 10, '0': {my: 'my value'}});
+  });
 }());
