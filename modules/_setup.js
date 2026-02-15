837ef7ecb4a8a1408a0a970035f475dac4da630c
Julian Gonggrijp <dev@juliangonggrijp.com>
2026-02-16 00:41:58 +0100

    Bump the version to 1.13.8

--- modules/_setup.js
+++ modules/_setup.js
@@ -1,5 +1,5 @@
 // Current version.
-export var VERSION = '1.13.7';
+export var VERSION = '1.13.8';
 
 // Establish the root object, `window` (`self`) in the browser, `global`
 // on the server, or `this` in some virtual machines. We use `self`
